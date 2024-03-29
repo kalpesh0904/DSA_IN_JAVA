83a9b5c0785f94e5b00d9b6475553795bf579ccd
kalpesh0904 <kalpesho0904@gmail.com>
2024-03-29 18:58:57 +0530

    updated code

--- DSAinJAVA/CodingBlocks/src/Arrays/Array_swap.java
+++ DSAinJAVA/CodingBlocks/src/Arrays/Array_swap.java
@@ -1,2 +1,15 @@
-package Arrays;public class Array_swap {
+package Arrays;
+
+public class Array_swap {
+    public static void main(String[] args){
+        int[] arr={10,20,30,40,50};
+        System.out.println(arr[0] + " "+arr[1]);
+        swap(arr[0],arr[1]);                       //Here we are swapping using local variable so we are not changeing arr
+        System.out.println(arr[0] + " "+arr[1]);
+    }
+    public static void swap(int a,int b){
+        int temp=a;
+        a=b;
+        b=temp;
+    }
 }
