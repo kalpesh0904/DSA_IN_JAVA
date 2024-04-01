ce375946c2b2c0026f42cba3d3fae2f7baf8e703
kalpesh0904 <kalpesho0904@gmail.com>
2024-04-01 21:33:04 +0530

    swaping elements of two arrays

--- DSAinJAVA/CodingBlocks/src/Arrays/Arrays_swap2.java
+++ DSAinJAVA/CodingBlocks/src/Arrays/Arrays_swap2.java
@@ -5,7 +5,7 @@ public class Arrays_swap2 {
         int[] arr={10,20,30,40,50};
         int[] other={100,200,300,400,500};
         System.out.println(arr[0] + " "+arr[1]);
-        swap(arr,other);                       //Here we are swapping using local variable so we are not changeing in main fn
+        swap(arr,other);
         System.out.println(arr[0] + " "+arr[1]);
     }
     public static void swap(int[] arr,int[] other){
