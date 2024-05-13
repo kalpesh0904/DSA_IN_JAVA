78ba23631932ec5fe8b119949ea6c4acd63811c3
kalpesh0904 <kalpesho0904@gmail.com>
2024-05-13 11:27:44 +0530

    array reverse using an extra array

---
+++ DSAinJAVA/CodingBlocks/src/Arrays/Reverse.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Reverse {
+    public static void main(String[] args){
+        int[] arr ={1,2,3,4,5};
+        int[] rev =new int[arr.length];
+        for(int i =0;i<arr.length;i++){
+            rev[i]=arr[arr.length-i-1];
+        }
+        for(int i =0;i<arr.length;i++) {
+            System.out.print(rev[i]+" ");
+        }
+    }
+}
